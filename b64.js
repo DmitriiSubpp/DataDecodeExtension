eaee6cd48aea9f175b09994b4fdb03e745456a79
DmitriiSubpp <71903279+DmitriiSubpp@users.noreply.github.com>
2021-11-20 21:57:15 +0300

    Add files via upload

--- b64.js
+++ b64.js
@@ -3,27 +3,35 @@
 let encodeBtn = document.getElementById('encodeBtn');
 let decodeBtn = document.getElementById('decodeBtn');
 let replaceBtn = document.getElementById('replaceBtn');
+let copyBtn = document.getElementById('copyBtn');
+let menu = document.getElementById('menu');
+
+
+// смена названий кнопок при выборе 'compare'
+
 
 encodeBtn.onclick = function() {
   let text_input  = document.getElementById('inp');
   let text_result = document.getElementById('res');
-  if (text_input.value) {  text_result.value = btoa(text_input.value); }
-  else { text_result.value = 'NULL'; }
+
+  if (menu.value == 'base64') text_result.value = btoa(text_input.value);
+  if (menu.value == 'url') text_result.value = encodeURI(text_input.value);
 }
 
 decodeBtn.onclick = function() {
   let text_input  = document.getElementById('inp');
   let text_result = document.getElementById('res');
-  if (text_input.value) {  text_result.value = atob(text_input.value); }
-  else { text_result.value = 'NULL'; }
-  text_input = text_result = '';
+
+  if (menu.value == 'base64') text_result.value = atob(text_input.value);
+  if (menu.value == 'url') text_result.value = decodeURI(text_input.value);
 }
 
 replaceBtn.onclick = function () {
   let text_input  = document.getElementById('inp');
   let text_result = document.getElementById('res');
-  if (text_input.value && text_input.value) {  [text_result.value, text_input.value] = [text_input.value, text_result.value]; }
-  else { text_result.value = text_input.value = 'NULL'; }
+  if (text_input.value && text_input.value)
+    [text_result.value, text_input.value] = [text_input.value, text_result.value];
+
 }
 
 clearBtn.onclick = function () {
@@ -31,9 +39,8 @@ clearBtn.onclick = function () {
   let text_result = document.getElementById('res');
   text_result.value = text_input.value = '';
 }
+
 copyBtn.onclick = function () {
-  alert("function in development!")
-  //let text_input  = document.getElementById('inp');
-  //let text_result = document.getElementById('res');
-  //text_result.value = text_input.value = '';
+  let text_input  = document.getElementById('inp');
+  text_input.value = "function in development!";
 }
